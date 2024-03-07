81472d1ba710ec48517289e70d9af9bef759e170
JayMartin05 <jay-martin@live.nmit.ac.nz>
2024-03-07 14:59:25 +1300

    end of first lesson (const,let.if,else)

--- Src/number.js
+++ Src/number.js
@@ -1,9 +1,37 @@
-//NOTE - the key word deacleard 'const' is locks the variable, the vairable name is 'num1' and '=' is assignment 10 is value of variable
-const num1 = 10
-console.log(num1)
-//NOTE - the key word decleared 'let' allows the given variable to change later on, num2 is the variable being a assigned the value of 20
-let num2 = 20
-console.log(num2)
-//NOTE - the key word decleared 'var' is an older version of let and const
-var num3 = 30
-console.log(num3)
+// //NOTE - the key word deacleard 'const' is locks the variable, the vairable name is 'num1' and '=' is assignment 10 is value of variable
+// const pi = 3.14
+// // pi = 10
+// console.log(pi)
+// //NOTE - the key word decleared 'let' allows the given variable to change later on, num2 is the variable being a assigned the value of 20
+// let num2 = 20
+// num2 = 25
+// console.log(num2)
+// //NOTE - the key word decleared 'var' is an older version of let and const
+// var num3 = 30
+// console.log(num3)
+
+// const fullName = "Jay Martin"
+// console.log(fullName)
+// const studyPlace = "NMIT"
+// console.log(studyPlace)
+// console.log(fullName + " " + studyPlace)
+
+// console.log(`My full name is ${fullName} and I study at ${studyPlace}`)
+
+// const working = true
+// const notWorking = false
+
+// if (pi == 3.14) {
+//     console.log(fullName)
+// }
+let a = 2
+let result
+// if() {} else {}  <-- thats how an if and else statement looks
+a = 0
+if (a >= 0) {
+    result = "postive";
+    console.log(result)
+} else {
+    result = "Not positive";
+    console.log(result)
+}
