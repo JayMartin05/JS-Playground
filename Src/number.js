53127d742814024f7ab5eb970315b31be79b7540
JayMartin05 <jay-martin@live.nmit.ac.nz>
2024-03-07 13:49:38 +1300

    Added notes and practiced console.log in Src/number.js

---
+++ Src/number.js
@@ -0,0 +1,9 @@
+//NOTE - the key word deacleard 'const' is locks the variable, the vairable name is 'num1' and '=' is assignment 10 is value of variable
+const num1 = 10
+console.log(num1)
+//NOTE - the key word decleared 'let' allows the given variable to change later on, num2 is the variable being a assigned the value of 20
+let num2 = 20
+console.log(num2)
+//NOTE - the key word decleared 'var' is an older version of let and const
+var num3 = 30
+console.log(num3)
