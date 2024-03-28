8ba631a38910bda112c8ba60b38783dbc0f855e2
JayMartin05 <jay-martin@live.nmit.ac.nz>
2024-03-28 14:37:31 +1300

    i still have no idea what im doing

--- index1.js
+++ index1.js
@@ -19,3 +19,56 @@ function arrayToNumbersThree(arr) {
 }
 
 console.log(arrayToNumbersThree(["1", "3", "6.7"]))
+
+// Create a function that return (true) if all parameters are thruthy and false otherwise
+//Ex: checkParam(true, true, true) -> true
+//Ex: checkParam(5, 1, 3, 0) -> false
+// Falsy values are: false, 0 and "" 
+
+//ES6
+const checkParam = (...args) => args.every(Boolean)
+console.log(checkParam(5))
+
+/*function checkParam(string) {
+    let value = false
+    for(let i = 0; i < string.length; i++) {
+        if(string !== 0 && string !== false && string !== "") {
+            value = true
+        }
+    }
+    return value
+}
+
+console.log(checkParam(5,1,3,1)) FUCKED*/
+
+//ES5
+function checkParamThree(param1, param2, param3, param4) {
+    if(param1 && param2 && param3 && param4) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(checkParamThree(5,1,3,1,7,10000, 0))
+
+// Create a function to check if an array contains a particular number
+//Ex: checkArray([5,5,6], 5) -> true
+//Ex: checkArray([1,3,5,7,10],2) -> false
+
+//ES6
+const checkArray = (arr, el) => {
+    return arr.includes(el)
+}
+
+console.log(checkArray([5,5,6], 6, 5))
+
+//ES5
+
+function checkArrayTwo (arr, num) {
+    if(arr.includes(num)) {
+        return true
+     /*else
+    return false*/
+}
+
+console.log(checkArrayTwo([5,5,6], 7))
